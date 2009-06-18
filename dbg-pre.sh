2a0bc9e820c525e68fc8f46fcdf2fdc000fc59d4
rocky <rocky@sanchez.(none)>
2009-06-18 08:04:29 -0400

    See previous commit

--- dbg-pre.sh
+++ dbg-pre.sh
@@ -1,7 +1,7 @@
 # -*- shell-script -*-
 # dbg-pre.sh - Code common to zshdb and zshdb-trace that has to run first
 #
-#   Copyright (C) 2008 Rocky Bernstein rocky@gnu.org
+#   Copyright (C) 2008, 2009 Rocky Bernstein rocky@gnu.org
 #
 #   zshdb is free software; you can redistribute it and/or modify it under
 #   the terms of the GNU General Public License as published by the Free
