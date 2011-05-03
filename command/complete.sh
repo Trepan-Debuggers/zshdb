aef6c0ba48127ae41c86bf21410be6e6788e29a7
rocky <rocky@gnu.org>
2011-05-03 00:32:41 -0400

    Fix up "complete" command correctly.

--- command/complete.sh
+++ command/complete.sh
@@ -17,38 +17,182 @@
 #   the Free Software Foundation, 59 Temple Place, Suite 330, Boston,
 #   MA 02111 USA.
 
+if [[ 0 == ${#funcfiletrace[@]} ]] ; then
+    # source ../init/require.sh 
+    # FIXME: require loses scope for typeset -A...
+    source ../lib/help.sh
+    source ../lib/alias.sh
+    setopt ksharrays
+fi
+
 _Dbg_help_add complete \
 'complete PREFIX-STR...
 
 Show command completion strings for PREFIX-STR
 '
 
-_Dbg_do_complete() {
-  typeset -a _Dbg_commands; _Dbg_commands=( - 
-	. / a break
-	cd commands complete continue condition clear
-	d debug delete disable display
-	D deleteall down eval enable examine
-	file finish frame 
-	handle help history info
-	list kill next step skip print pwd quit reverse
-	search set show signal source toggle tbreak tty
-	up undisplay watche version window 
-	A x L M R S T We )
+#### zsh compgen's -W (words) doesn't match bash's. 
+#### Until we get a fix into zsh....
 
+## Uncomment after we get a change to zsh...
+## autoload -Uz bashcompinit
+## bashcompinit
+## zmodload -ap zsh/parameter parameters
+
+select_words() {
+    typeset -a words
+    eval "words=( $1 )"
+    local find
+    find=$2
+    local try
+    find=${@[-1]}
+    for try in ${words[@]} ; do 
+	if [[ $try =~ "^$find" ]] ; then 
+	    results+=( $try )
+	fi
+    done
+    results=( "${(ko)results[@]}" )
+}
+
+compgen() {
+  local -a results    
+  local opts prefix suffix job OPTARG OPTIND ret=1 
+  local -a name res results jids
+  local -A shortopts
+  
+  emulate -L sh
+  setopt kshglob noshglob braceexpand nokshautoload
+
+  shortopts=(
+    a alias b builtin c command d directory e export f file
+    g group j job k keyword u user v variable
+  )
+
+  while getopts "o:A:G:C:F:P:S:W:X:abcdefgjkuv" name; do
+    case $name in
+      [abcdefgjkuv]) OPTARG="${shortopts[$name]}" ;&
+      A)
+        case $OPTARG in
+	  alias) results+=( "${(k)aliases[@]}" ) ;;
+	  arrayvar) results+=( "${(k@)parameters[(R)array*]}" ) ;;
+	  binding) results+=( "${(k)widgets[@]}" ) ;;
+	  builtin) results+=( "${(k)builtins[@]}" "${(k)dis_builtins[@]}" ) ;;
+	  command)
+	    results+=(
+	      "${(k)commands[@]}" "${(k)aliases[@]}" "${(k)builtins[@]}"
+	      "${(k)functions[@]}" "${(k)reswords[@]}"
+	    )
+	  ;;
+	  directory)
+	    setopt bareglobqual
+	    results+=( ${IPREFIX}${PREFIX}*${SUFFIX}${ISUFFIX}(N-/) )
+	    setopt nobareglobqual
+	  ;;
+	  disabled) results+=( "${(k)dis_builtins[@]}" ) ;;
+	  enabled) results+=( "${(k)builtins[@]}" ) ;;
+	  export) results+=( "${(k)parameters[(R)*export*]}" ) ;;
+	  file)
+	    setopt bareglobqual
+	    results+=( ${IPREFIX}${PREFIX}*${SUFFIX}${ISUFFIX}(N) )
+	    setopt nobareglobqual
+	  ;;
+	  function) results+=( "${(k)functions[@]}" ) ;;
+	  group)
+	    emulate zsh
+	    _groups -U -O res
+	    emulate sh
+	    setopt kshglob noshglob braceexpand
+	    results+=( "${res[@]}" )
+	  ;;
+	  hostname)
+	    emulate zsh
+	    _hosts -U -O res
+	    emulate sh
+	    setopt kshglob noshglob braceexpand
+	    results+=( "${res[@]}" )
+	  ;;
+	  job) results+=( "${savejobtexts[@]%% *}" );;
+	  keyword) results+=( "${(k)reswords[@]}" ) ;;
+	  running)
+	    jids=( "${(@k)savejobstates[(R)running*]}" )
+	    for job in "${jids[@]}"; do
+	      results+=( ${savejobtexts[$job]%% *} )
+	    done
+	  ;;
+	  stopped)
+	    jids=( "${(@k)savejobstates[(R)suspended*]}" )
+	    for job in "${jids[@]}"; do
+	      results+=( ${savejobtexts[$job]%% *} )
+	    done
+	  ;;
+	  setopt|shopt) results+=( "${(k)options[@]}" ) ;;
+	  signal) results+=( "SIG${^signals[@]}" ) ;;
+	  user) results+=( "${(k)userdirs[@]}" ) ;;
+      	  variable) results+=( "${(k)parameters[@]}" ) ;;
+	  helptopic) ;;
+	esac
+      ;;
+      F)
+        COMPREPLY=()
+	$OPTARG "${words[0]}" "${words[CURRENT-1]}" "${words[CURRENT-2]}"
+	results+=( "${COMPREPLY[@]}" )
+      ;;
+      G)
+        setopt nullglob
+	select_words "${~OPTARG}" "${@[-1]}"
+	unsetopt nullglob
+      ;;
+      W) select_words "$OPTARG" "${@[-1]}"
+      ;;
+      C) results+=( $(eval $OPTARG) ) ;;
+      P) prefix="$OPTARG" ;;
+      S) suffix="$OPTARG" ;;
+      X)
+        if [[ ${OPTARG[0]} = '!' ]]; then
+	  results=( "${(M)results[@]:#${OPTARG#?}}" )
+	else
+ 	  results=( "${results[@]:#$OPTARG}" )
+	fi
+      ;;
+    esac
+  done
+#### End fix stuff.
+  
+  # support for the last, `word' option to compgen. Zsh's matching does a
+  # better job but if you need to, comment this in and use compadd -U
+  #shift $(( OPTIND - 1 ))
+  #(( $# )) && results=( "${(M)results[@]:#$1*}" )
+
+  print -l -- "$prefix${^results[@]}$suffix"
+}
+
+_Dbg_do_complete() {
     typeset -a args; args=($@)
     _Dbg_matches=()
     if (( ${#args[@]} == 2 )) ; then
-      _Dbg_subcmd_complete ${args[0]} ${args[1]}
+	_Dbg_subcmd_complete ${args[0]} ${args[1]}
     elif (( ${#args[@]} == 1 )) ; then 
-	_Dbg_msg "complete command not completed yet."
-	# FIXME: figure out zsh's equivalent for:
-	# eval "builtin compgen -W \"${_Dbg_commands[@]}\" ${args[0]}"
-	:
+	typeset -a ary
+	ary=(${(k)_Dbg_debugger_commands[@]})
+	IFS=' ' list=${ary[@]}
+	compgen -W "${list}" "${args[0]}"
     fi  
     typeset -i i
     for (( i=0;  i < ${#_Dbg_matches[@]}  ; i++ )) ; do 
-      _Dbg_msg ${_Dbg_matches[$i]}
+	_Dbg_msg ${_Dbg_matches[$i]}
     done
 }
 
+# Demo it.
+if [[ 0 == ${#funcfiletrace[@]} ]] ; then
+    source ./help.sh 
+    source ../lib/msg.sh 
+    _Dbg_libdir='..'
+    for _Dbg_file in ${_Dbg_libdir}/command/c*.sh ; do 
+    	source $_Dbg_file
+    done
+
+    _Dbg_args='complete'
+    _Dbg_do_help complete
+    _Dbg_do_complete c
+fi
