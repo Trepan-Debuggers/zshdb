f5685148140f17e5cd0dee35dc2ac05ef284c74e
rocky <rocky@gnu.org>
2011-05-15 20:35:27 -0400

    DRY unit tests by using a helper file.

---
+++ test/unit/helper.sh
@@ -0,0 +1,10 @@
+PS4='(%x:%I): [%?] zsh+ 
+'
+setopt ksharrays
+set -o shwordsplit
+shunit_file=${abs_top_srcdir}test/unit/shunit2
+_Dbg_libdir=$abs_top_srcdir
+shunit_file=${abs_top_srcdir}test/unit/shunit2
+
+# Don't need to show banner
+set -- '-q'  
