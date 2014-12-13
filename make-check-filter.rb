47fbac8d74a0bcfd6e8a21e2527172f9dc223d91
rocky <rocky@gnu.org>
2014-12-12 23:28:49 -0500

    Makefile.am: use remake comment targets. Add check-short target check-common.sh.in: split typeset and assignment which makes it work on a Solaris environment

---
+++ make-check-filter.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+# Use this to cut out the crud from make check.
+# Use like this:
+#   make check 2>&1  | ruby ../make-check-filter.rb
+# See Makefile.am
+pats = ["^(?:Loading",
+        '(re)?make\[',
+        "Making check in",
+        '\(cd \.\.',
+        "make -C",
+        "Test-Unit",
+        "config.status",
+        "`flet'",
+        '\s*$',
+        '##[<>]+$'
+       ].join('|') + ')'
+# puts pats
+skip_re = /#{pats}/
+
+while gets()
+  next if $_.encode!('UTF-8', 'binary',
+                     invalid: :replace, undef: :replace, replace: '') =~ skip_re
+  puts $_
+end
