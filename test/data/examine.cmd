0def8c25adeac2be0efc1bd7051ee45b197f095b
rocky <rocky@gnu.org>
2015-12-19 11:50:33 -0500

    Handle both print forms (1,2,3) and ( 1, 2, 3) for typeset -p array

--- test/data/examine.cmd
+++ test/data/examine.cmd
@@ -1,7 +1,7 @@
 set trace-commands on
 # Examine with blanks
-x 
-x         
+x
+x
 # Examine constant expressions
 x 1+30
 x '2*4+10/2'
@@ -9,7 +9,6 @@ x '(2*4+10)/2'
 x "1<<4"
 # Set up some values
 continue 10
-x ary
 x string
 x xstring
 x fn
