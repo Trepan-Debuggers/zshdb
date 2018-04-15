b811e3d94d56c562de5564a6441fa1cedf66b874
rocky <rocky@gnu.org>
2018-04-15 12:00:30 -0400

    Bump copyright

--- lib/gdb.sh
+++ lib/gdb.sh
@@ -1,6 +1,6 @@
 # -*- shell-script -*-
 # routines that seem tailored more to the gdb-style of doing things.
-#   Copyright (C) 2008, 2011, 2015 Rocky Bernstein <rocky@gnu.org>
+#   Copyright (C) 2008, 2011, 2015, 2018 Rocky Bernstein <rocky@gnu.org>
 #
 #   This program is free software; you can redistribute it and/or
 #   modify it under the terms of the GNU General Public License as
