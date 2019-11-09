342beda11076c23d92ebf4bd4c486a93498a8d03
rocky <rocky@gnu.org>
2019-11-08 22:24:15 -0500

    Remove a derived file

--- test/example/bug-args.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/zsh
-echo Dollar 0 is $(basename $0)
-echo First parm is: $1
-set a b c d e
-shift 2
-# At this point we shouldn't have a $5 or a $4
-0="foo"
-echo Dollar 0 is now $0
-exit 0
