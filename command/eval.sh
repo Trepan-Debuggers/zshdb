9718e8eebbcfc4df7f5ebbc216006d812614c82a
Roland Christmann <roland.christmann@gmail.com>
2021-07-17 17:18:30 +0200

    add the return code of the evaluated expression

--- command/eval.sh
+++ command/eval.sh
@@ -117,6 +117,7 @@ _Dbg_do_eval() {
         _Dbg_set_dol_q $_Dbg_debugged_exit_code
         . $_Dbg_evalfile
     fi
+    _Dbg_rc=$?
     (( _Dbg_show_eval_rc )) && _Dbg_msg "\$? is $_Dbg_rc"
     # We've reset some variables like IFS and PS4 to make eval look
     # like they were before debugger entry - so reset them now.
