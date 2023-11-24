33f3c32049171320cc0db65340b862f7046e8cf1
Joachim Ansorg <github@ja-dev.eu>
2023-11-24 19:02:22 +0100

    Move "info stack" command into its own file

---
+++ command/info_sub/stack.sh
@@ -0,0 +1,26 @@
+# -*- shell-script -*-
+# "info stack" debugger command
+#
+#   Copyright (C) 2023 Rocky Bernstein rocky@gnu.org
+#
+#   zshdb is free software; you can redistribute it and/or modify it under
+#   the terms of the GNU General Public License as published by the Free
+#   Software Foundation; either version 2, or (at your option) any later
+#   version.
+#
+#   zshdb is distributed in the hope that it will be useful, but WITHOUT ANY
+#   WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+#   for more details.
+#
+#   You should have received a copy of the GNU General Public License along
+#   with zshdb; see the file COPYING.  If not, write to the Free Software
+#   Foundation, 59 Temple Place, Suite 330, Boston, MA 02111 USA.
+
+_Dbg_help_add_sub info stack '
+**info stack**
+' 1
+
+_Dbg_do_info_stack() {
+    _Dbg_do_backtrace 1 $@
+}
